ea571cc0f294e7cd45b4921c9be92a5e33801472
KauaMB2 <i.kauamoreirabatista@gmail.com>
2026-01-13 21:57:27 -0300

    bug of print messages were fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { Canvas } from "@react-three/fiber";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import { Leva } from "leva";
-import { useState } from "react";
 import { DEFAULT_CAMERA_POSITION } from "./components/CameraManager.js";
 import { Experience } from "./components/Experience.js";
 import { UI } from "./components/UI.js";
 import { MobileWarning, useMobileDetection } from "./components/MobileWarningComponent.js";
+import { useState } from "react";
 
 function App() {
   const isMobile = useMobileDetection();
   const [showWarning, setShowWarning] = useState(true);
-
   if (isMobile && showWarning) {
     return <MobileWarning onContinue={async () => setShowWarning(false)} />;
   }
@@ -18,16 +17,26 @@ function App() {
   return (
     <>
       <Leva hidden />
-      <Canvas camera={{ position: DEFAULT_CAMERA_POSITION, fov: 42 }}>
-        <color attach="background" args={["#171720"]} />
-        <fog attach="fog" args={["#171720", 60, 90]} />
-        <ambientLight intensity={0.2} />
-        <Experience />
+      <UI />
+      <Canvas
+        camera={{
+          position: DEFAULT_CAMERA_POSITION,
+          fov: 45,
+        }}
+        gl={{
+          preserveDrawingBuffer: true,
+        }}
+        shadows
+      >
+        <color attach="background" args={["#130f30"]} />
+        <fog attach="fog" args={["#130f30", 10, 40]} />
+        <group position-y={-1}>
+          <Experience />
+        </group>
         <EffectComposer>
-          <Bloom mipmapBlur luminanceThreshold={1} intensity={1.42} />
+          <Bloom mipmapBlur luminanceThreshold={1.2} intensity={1.2} />
         </EffectComposer>
       </Canvas>
-      <UI />
     </>
   );
 }
